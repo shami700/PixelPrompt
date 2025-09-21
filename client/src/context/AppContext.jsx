844d63af3b23a073a583fc8914aa5bb0276913d8
MdShami <shamiarzoo2@gmail.com>
2025-09-21 16:00:32 +0530

    Okieeee

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -11,7 +11,7 @@ const AppContextProvider = (props) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [credit, setCredit] = useState(false);
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "";
   const navigate = useNavigate()
 
   const loadCreditsData = async () => {
