755b269f5c31e465855ef56b4f05f0352c82f2b4
MdShami <shamiarzoo2@gmail.com>
2025-09-21 14:54:53 +0530

    Deploy ready

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("Database Connected"));
-  await mongoose.connect(`${process.env.MONGODB_URI}`);
+  await mongoose.connect(`mongodb+srv://Shami:shami%4012345@pixelprompt.54ej6a0.mongodb.net/?retryWrites=true&w=majority&appName=PixelPrompt`);
 };
 
 export default connectDB;
